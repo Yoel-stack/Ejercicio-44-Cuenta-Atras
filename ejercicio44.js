bdadb99a081f15f44503f0e35557a985b69eefd4
Yoel <yoelmontelongoo0@gmail.com>
2024-11-17 13:55:29 -0300

    Add files via upload

---
+++ ejercicio44.js
@@ -0,0 +1,49 @@
+/*
+ * EJERCICIO:
+ * ¡El 12 de noviembre lanzo mouredev pro!
+ * El campus de la comunidad para estudiar programación de
+ * una manera diferente: https://mouredev.pro
+ *
+ * Crea un programa que funcione como una cuenta atrás.
+ *
+ * - Al iniciarlo tendrás que indicarle el día, mes, año,
+ *   hora, minuto y segundo en el que quieres que finalice.
+ * - Deberás transformar esa fecha local a UTC.
+ * - La cuenta atrás comenzará y mostrará los días, horas,
+ *   minutos y segundos que faltan.
+ * - Se actualizará cada segundo y borrará la terminal en
+ *   cada nueva representación del tiempo restante.
+ * - Una vez finalice, mostrará un mensaje.
+ * - Realiza la ejecución, si el lenguaje lo soporta, en
+ *   un hilo independiente.
+ */
+
+
+const days = document.getElementById("days")
+const hours = document.getElementById("hours")
+const minutes = document.getElementById("minutes")
+const seconds = document.getElementById("seconds")
+
+//Fecha de finalizacion cuenta atras
+const navidad = `25 Dec 2024` 
+
+function cuentaAtras(){
+    const navidadDate = new Date(navidad);
+    const fechaActual = new Date();
+    const totalSec = (navidadDate - fechaActual) / 1000;
+
+    const totalDays = Math.floor(totalSec / 3600 / 24);
+    const totalHours = Math.floor(totalSec / 3600) % 24;
+    const totalMinutes = Math.floor(totalSec / 60) % 60;
+    const totalSeconds = Math.floor(totalSec % 60);
+
+    console.log(totalDays, totalHours, totalMinutes, totalSeconds);
+
+    days.innerHTML = totalDays;
+    hours.innerHTML = totalHours;
+    minutes.innerHTML = totalMinutes;
+    seconds.innerHTML = totalSeconds;
+
+};
+cuentaAtras(setInterval(cuentaAtras, 1000));
+
